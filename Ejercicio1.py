cedaf38bac532b3c33cab193f7173179e362a14d
Eddieee2 <lescoe1234@gmail.com>
2025-04-03 19:36:45 -0600

    main

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,7 +2,10 @@ temp_fah = float(input("Ingresa la temperatura en F: "))
 temp_cel = temp_fah - 32
 temp_cel = temp_cel * 5
 temp_cel = temp_cel / 9
+
 print("La temperatura en Celsius es: ", temp_cel)
+
 temp_kel = temp_cel + 273.15
+
 print(f"Grados Celcius {temp_cel:.2f}")
 print(f"Grados Kelvin {temp_kel:.2f}")
--- Ejercicio2.py
+++ Ejercicio2.py
@@ -3,7 +3,7 @@ base = float(input("Ingrese la base del rectángulo: "))
 altura = float(input("Ingrese la altura del rectángulo: "))
 
 area = base * altura
-
+1
 perimetro_base = base * 2
 
 perimetro_altura = altura * 2
@@ -11,5 +11,4 @@ perimetro_altura = altura * 2
 perimetro = perimetro_base + perimetro_altura 
 
 print(f"El área del rectángulo es: {area}")
-
 print(f"El perímetro del rectángulo es: {perimetro}")
